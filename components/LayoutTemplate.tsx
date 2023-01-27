3bd10113a06ce0e5a5d0aa6ffc9f45c12b1218f5
StrixSC <talon.strix@gmail.com>
2023-01-27 01:26:34 -0500

    hotfix: fixed bug where ganalytics wasnt being loaded properly due to html encoding

--- components/LayoutTemplate.tsx
+++ components/LayoutTemplate.tsx
@@ -13,6 +13,13 @@ const LayoutTemplate = (props: any) => {
         ...customMeta
     };
 
+    const gscriptData = `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', "${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}");
+    `;
+
     const router = useRouter();
     return (
         <div
@@ -37,12 +44,7 @@ const LayoutTemplate = (props: any) => {
                 {meta.date && <meta property="article:published_time" content={meta.date} />}
                 <script
                     src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}></script>
-                <script>{`
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                    gtag('config', "${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}");
-                `}</script>
+                <script dangerouslySetInnerHTML={{ __html: gscriptData }}></script>
             </Head>
             <nav>
                 <Nav></Nav>
