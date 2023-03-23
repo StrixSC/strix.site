1338f431465b706b502f4a5200b3bd89ee67b782
StrixSC <talon.strix@gmail.com>
2023-03-22 21:48:50 -0400

    feat: changed resume pages to links to resumes

--- components/CVButton.tsx
+++ components/CVButton.tsx
@@ -11,8 +11,14 @@ const CVButton = () => {
             </NavItem>
             {dropDownState && (
                 <div className="absolute right-0 flex flex-col items-center mt-2 bg-neutral-100 dark:bg-neutral-800">
-                    <NavItem className="w-full" href="/resume_fr.html" text="French"></NavItem>
-                    <NavItem className="w-full" text="English" href="/resume.html"></NavItem>
+                    <NavItem
+                        className="w-full"
+                        href="https://drive.google.com/file/d/1T-yIUZ2VBmnIAI5G-kIAZJ5Lv39_VRcw/view?usp=share_link"
+                        text="French"></NavItem>
+                    <NavItem
+                        className="w-full"
+                        text="English"
+                        href="https://drive.google.com/file/d/1SobHiTwJjF-FCJpIgA7Z7vfiRcJK032R/view?usp=share_link"></NavItem>
                 </div>
             )}
         </div>
